95f86eb6912d935f2808599803f91376f3196466
Tanaporn025 <istudents@iClassrooms-iMac-37.local>
2015-01-16 16:42:13 +0700

    looplab

--- Arraylabtest.playground/section-1.swift
+++ Arraylabtest.playground/section-1.swift
@@ -2,17 +2,17 @@
 
 import UIKit
 
-var str = "Hello, playground"
 
 var shoppingListExplicit = [String]()
 shoppingListExplicit = ["Eggs","Milk"]
 // var shoppingListExplict: [String] = ["Eggs", "Milk"]
 
-var shoppingList =["Eggs","Milk"]
-println("The shopping list contains \(shoppingList.count)items.")
+var shoppingList = ["Eggs","Milk"]
+println("The shopping list contains \(shoppingList.count) items.")
 shoppingList.append("Flour")
 shoppingList += ["Baking Powder"]
 shoppingList += ["Chocolate Spread","Cheese","Butter"]
 var firstItem = shoppingList[0]
 shoppingList[0] = "Six eggs"
 shoppingList
+
---
+++ looplab.playground/section-1.swift
@@ -0,0 +1,16 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+
+
+var array1 = ["abc","bcd","cgf"]
+for (var i = 0 ; i < array1.count;i++) {
+        println(array1[1])
+    }
+    
+    var array2 = ["a","b","c"]
+    var str = ""
+for str in array2 {
+    println(str)
+}
