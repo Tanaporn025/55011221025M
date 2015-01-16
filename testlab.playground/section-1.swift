50beb23669091950ef9dce436ec18297d1a42cf5
Tanaporn025 <istudents@iClassrooms-iMac-37.local>
2015-01-16 15:53:37 +0700

    lab

---
+++ testlab.playground/section-1.swift
@@ -0,0 +1,28 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground,"
+
+let tutorialTeam = 56
+let editoriaTeam = 23
+var totalTeam = tutorialTeam + editoriaTeam
+
+totalTeam += 1
+let priceIntInferred = -19
+let priceIntExplicit:Int = -19
+
+let priceIntferred = -19.99
+let priceExplicit:Double = -19.99
+
+let priceFloatInferred = -19.99
+let priceFloatExplict:Float = -19.99
+
+let onSaleInferred = true
+let onSaleExplicit:Bool = false
+
+let nameInferred = "whoopie Cushion"
+let nameExplicit:String = "whoopie Cushion"
+
+
+
